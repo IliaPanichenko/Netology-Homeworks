be1b9a5948acf0ff6883c1e3fc637b67362b3927
Ilia Panichenko <ilia.panichenko@abbyy.com>
2024-07-29 17:47:40 +0200

    Unique_ptr

---
+++ Advanced_programming_C++/Smart_pointers/Task3_Stdunique_ptr/Stdunique_ptr/Stdunique_ptr.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+template < typename T>
+class My_unique_ptr
+{
+private:
+	T* m_ptr;
+
+public:
+	My_unique_ptr(T* ptr = nullptr) : m_ptr(ptr) {};
+	T& operator*() const
+	{
+		return *m_ptr;
+	}
+	T* operator->() const
+	{
+		return m_ptr;
+	}
+	T* release()
+	{
+		T* temp = m_ptr;
+		m_ptr = nullptr;
+		return temp;
+	}
+	My_unique_ptr(const My_unique_ptr& other) = delete;
+	My_unique_ptr& operator=(const My_unique_ptr& other) = delete;
+	~My_unique_ptr()
+	{
+		delete m_ptr;
+	}
+};
+int main()
+{
+	My_unique_ptr<int> ptr(new int(20));
+	std::cout << "Value: " << *ptr << "\n";
+	*ptr = 15;
+	std::cout << "Value after assignment: " << *ptr << "\n";
+	int* rawPtr = ptr.release();
+	std::cout << "Raw pointer: " << rawPtr << "\n";
+	return 0;
+}
