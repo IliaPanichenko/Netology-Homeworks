d65ec5c4ae18719ec2a6272aada1041b58db78b9
Ilia <ilyapanichenko@gmail.com>
2024-10-21 22:41:33 +0200

    Lesson3_Asynk

---
+++ Multithreaded_and_Asynchronous_Programming/Async_programming/Task1_Selection_sort/Task1_Selection_sort.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <future>
+#include <chrono>
+
+int findMin(std::vector<int>& arr, int start, int end, std::promise<int> promise)
+{
+	int min_ind = start;
+	for (int i = start + 1; i < end; i++)
+	{
+		if (arr[min_ind] > arr[i])
+		{
+			min_ind = i;
+		}
+	}
+	promise.set_value(min_ind);
+	return min_ind;
+}
+
+void sortArray(std::vector<int>& arr)
+{
+	for (int j = 0; j < arr.size() - 1; j++)
+	{
+		std::promise<int> promise;
+		std::future<int> future = promise.get_future();
+		std::async(std::launch::async, findMin, std::ref(arr), j, arr.size(), std::move(promise));
+		int min_ind = future.get();
+		if (j != min_ind) 
+		{
+		    std::swap(arr[j], arr[min_ind]);
+        }
+	}
+}
+
+void printArr(std::vector<int>& arr)
+{
+	for (int ellem : arr)
+	{
+		std::cout << ellem << " ";
+	}
+	std::cout << "\n";
+}
+
+void fillArr(std::vector<int> & arr, int min_value, int max_value)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(min_value, max_value);
+	for (int& elem : arr) {
+		elem = dist(gen);
+	}
+}
+
+int main() {
+    int size;
+        std::cout << "Input array size: ";
+        std::cin >> size;
+    std::vector<int> arr(size);
+	fillArr(arr, 1, 100);
+    std::cout << "Origin array: ";
+	printArr(arr);
+	sortArray(arr);
+    std::cout << "Sorted array: ";
+	printArr(arr);
+    return 0;
+}
---
+++ Multithreaded_and_Asynchronous_Programming/Async_programming/Task2_For_each/Task2_For_each.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <algorithm>
+
+template<typename Iterator, typename Func>
+void parallelFor_each(Iterator begin, Iterator end, Func func)
+{
+    int distance = std::distance(begin, end);
+    if (distance <= 1)
+    {
+        if (begin != end)
+        {
+            func(*begin);
+        }
+        return;
+    }
+    Iterator mid = std::next(begin, distance / 2);
+    auto future = std::async(std::launch::async, parallelFor_each<Iterator, Func>, begin, mid, func);
+    parallelFor_each(mid, end, func);
+    future.get();
+}
+
+int main()
+{
+    std::vector<int> vec(10, 1);
+    auto increment = [](int& x) {x += 1; };
+    parallelFor_each(vec.begin(), vec.end(), increment);
+    std::cout << "After increment: ";
+    for (const auto elem : vec)
+    {
+        std::cout << elem << " ";
+    }
+    return 0;
+}
